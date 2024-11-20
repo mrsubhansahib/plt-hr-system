acf3a67bda4f1d1748c878a2652418e6e274f6fb
mrsubhanshaib <mr.subhansahib@gmail.com>
2024-11-20 12:50:40 +0500

    Updated Web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes For Web Application
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
