6f1081ce92d274ef275c9fdd62e29d484a471aba
mrsubhanshaib <mr.subhansahib@gmail.com>
2025-06-12 15:50:36 +0500

    select2 added

--- resources/views/pages/reports/emergency-info.blade.php
+++ resources/views/pages/reports/emergency-info.blade.php
@@ -1,6 +1,9 @@
 @extends('layout.master')
 
 @push('style')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css"
+        integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     @livewireStyles
     <style>
         label {
@@ -17,12 +20,50 @@
         </ol>
     </nav>
     @livewire('emergency-info')
+    <style>
+        .select2-container--default .select2-selection--single {
+            height: 37px !important;
+        }
+
+        .select2-container--default .select2-selection--single .select2-selection__arrow {
+
+            top: 6px !important;
+        }
+
+        .select2-container--default .select2-selection--single .select2-selection__rendered {
+            padding-top: 5px !important;
+        }
+
+        .select2-container--default .select2-selection--single .select2-selection__clear {
+            display: none;
+        }
+    </style>
 @endsection
 
 @push('custom-scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"
+        integrity="sha512-2ImtlRlf2VVmiGZsjm9bEyhjGW4dU7B6TNwh/hx/iSByxNENtj3WVE6o/9Lj4TJeVXPi4bnOIMXFIJJAeufa0A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     @livewireScripts
     <script>
         document.addEventListener('livewire:load', function() {
+            function initSelect2Sync() {
+                $('#emergency_info').select2({
+                    width: '100%',
+                    placeholder: 'Select an employee',
+                    allowClear: true
+                });
+
+                $('#emergency_info').on('change', function(e) {
+                    const value = $(this).val();
+                    let livewireComponent = Livewire.find(document.querySelector('[wire\\:id]')
+                        .getAttribute('wire:id'));
+                    livewireComponent.set('employee_id', value);
+                });
+            }
+
+            initSelect2Sync();
             // Initialize the DataTable with optimized options
             var table = $('.dataTableColleagues').DataTable({
                 autoWidth: false, // Prevent table from stretching
@@ -50,6 +91,8 @@
             Livewire.hook('message.processed', (message, component) => {
                 $('.dataTableColleagues').DataTable().destroy();
 
+                $('#emergency_info').select2('destroy');
+                initSelect2Sync();
                 // Initialize the DataTable with optimized options
                 var table = $('.dataTableColleagues').DataTable({
                     autoWidth: false, // Prevent table from stretching
